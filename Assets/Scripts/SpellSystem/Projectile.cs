2240b498946efa9e169c39a57935dcfe0e6c2f6c
WIN-TUH46MF7JO4\user <nikita1995f@gmail.com>
2024-04-02 18:16:28 +0300

    Update Fire Projectile spell for apply damage to enemy; Create damage projectile component; Add new Projectile layer, for disable collision with player;

---
+++ Assets/Scripts/SpellSystem/Projectile.cs
@@ -0,0 +1,30 @@
+using Enemies;
+using UnityEngine;
+
+namespace SpellSystem
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] private float speed = 10f;
+        
+        private float _damage;
+
+        public void Initialize(float damage) => _damage = damage;
+
+        private void Update() => MoveForward();
+
+        private void MoveForward()
+        {
+            var translation = Vector3.forward * speed * Time.deltaTime;
+            transform.Translate(translation);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<IDamageable>(out var damageable))
+                damageable.ApplyDamage(_damage);
+            
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpellSystem/ProjectileSpell.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace SpellSystem
+{
+    [CreateAssetMenu(menuName = "Nekki.TestTask/SpellSystem/Projectile Spell", fileName = "New Projectile Spell")]
+    public class ProjectileSpell : SpellBase
+    {
+        [SerializeField] private Projectile projectilePrefab;
+        
+        public override void Cast(Transform castTransform)
+        {
+            var projectile = Instantiate(projectilePrefab, castTransform.position, castTransform.rotation);
+            projectile.Initialize(damage);
+        }
+    }
+}
